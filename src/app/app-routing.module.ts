08085bda1d6917bc0676cd77561d63a29098a1b6
CHATBI Jaouad <j.chatbi@mmsp.gov.ma>
2018-11-09 16:52:12 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AdminLayoutComponent } from './_layout/admin-layout/admin-layout.component';
+import { PublicLayoutComponent } from './_layout/public-layout/public-layout.component';
+
+import { HomeComponent } from './admin/home/home.component';
+import { AboutComponent } from './public/about/about.component';
+import { LoginComponent } from './public/login/login.component';
+
+const routes: Routes = [
+
+  //Admin routes goes here 
+  { 
+    path: '', 
+    component: AdminLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent, pathMatch: 'full'},
+      { path: 'home', component: HomeComponent },
+    ]
+  },
+
+  // App routes goes here here
+  { 
+    path: '',
+    component: PublicLayoutComponent, 
+    children: [
+      { path: 'dashboard', component: AboutComponent },
+    ]
+  },
+
+  //no layout routes
+  { path: 'login', component: LoginComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './_layout/admin-layout/admin-layout.component';
+import { PublicLayoutComponent } from './_layout/public-layout/public-layout.component';
+import { HomeComponent } from './admin/home/home.component';
+import { AboutComponent } from './public/about/about.component';
+import { LoginComponent } from './public/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLayoutComponent,
+    PublicLayoutComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
